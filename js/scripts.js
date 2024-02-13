d8ffd4fe75173483307d8b4513cd586bd1c60b5c
ExeVizuara <vizuara01@gmail.com>
2024-02-13 10:12:07 -0300

    Ultimas correcciones en scripts

--- js/scripts.js
+++ js/scripts.js
@@ -114,22 +114,22 @@ sr.reveal('.services__card, .projects__card', {interval: 100})
 
 
 // -SCROLL ACTIVAR SECCIONES-
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () =>{
-    const scrollDown = windows.scrollY
+// const scrollActive = () =>{
+//     const scrollDown = windows.scrollY
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-                sectionTop = current.offsetTop -58,
-                sectionId = current.getAttribute('id'),
-                sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight,
+//                 sectionTop = current.offsetTop -58,
+//                 sectionId = current.getAttribute('id'),
+//                 sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-            sectionsClass.classList.add('active-link')
-        }else{
-            sectionsClass.classList.remove('active-link')
-        }
-    })
-}
-windows.addEventListener('scroll', scrollActive)
+//         if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+//             sectionsClass.classList.add('active-link')
+//         }else{
+//             sectionsClass.classList.remove('active-link')
+//         }
+//     })
+// }
+// windows.addEventListener('scroll', scrollActive)
